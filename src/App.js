bed5dcaf8326fe492d0325820acbdc4a9e3acd62
Antonio H. Banze <antoniohbanze@gmail.com>
2022-10-28 19:12:28 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,87 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2020 Metrological
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Lightning, Utils } from '@lightningjs/sdk'
+export default class App extends Lightning.Component {
+  static getFonts() {
+    return [{ family: 'Regular', url: Utils.asset('fonts/Roboto-Regular.ttf') }]
+  }
+
+  static _template() {
+    return {
+      Background: {
+        w: 1920,
+        h: 1080,
+        color: 0xfffbb03b,
+        src: Utils.asset('images/background.png'),
+      },
+      Logo: {
+        mountX: 0.5,
+        mountY: 1,
+        x: 200,
+        y: 600,
+        w: 200,
+        h: 200,
+        src: Utils.asset('images/logo.jpg'),
+      },
+      Text: {
+        mount: 0.5,
+        x: 600,
+        y: 720,
+        text: {
+          text: 'Bem-Vindo ao Geo Shapes!',
+          fontFace: 'Regular',
+          fontSize: 64,
+          textColor: 0xbbffffff,
+        },
+      },
+      RectangleDefault: {
+        x: 500,
+        y: 100,
+        w: 200,
+        h: 100,
+        rect: true,
+      },
+      EllipseDefaut: {
+        h: 140,
+        w: 500,
+        x: 200,
+        y: 80,
+        r: 100,
+        s: 50,
+      },
+    }
+  }
+
+  _init() {
+    this.tag('Background')
+      .animation({
+        duration: 15,
+        repeat: -1,
+        actions: [
+          {
+            t: '',
+            p: 'color',
+            v: { 0: { v: 0xfffbb03b }, 0.5: { v: 0xfff46730 }, 0.8: { v: 0xfffbb03b } },
+          },
+        ],
+      })
+      .start()
+  }
+}
